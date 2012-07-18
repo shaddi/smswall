2b2aa8f907410252deb7ac1cc9f0292345f0fca8
Shaddi Hasan <shaddih@gmail.com>
2012-07-18 00:52:17 -0700

    Add most list management and pending action work

--- smswall.py
+++ smswall.py
@@ -8,40 +8,81 @@ class List:
         self.shortcode = shortcode
         self.app = app # the SMSWall app that made this List object
         self.conf = app.conf
-        self.db = self.conf.db
+        self.db = self.conf.db_conn
 
-    def create(self):
-        if not app.is_valid_shortcode(self.shortcode):
-            app.reply("The shortcode you selected is invalid. Please choose a" +
-                      + "number between %d and %d." % (self.conf.min_shortcode, \
-                                                       self.conf.max_shortcode))
-        elif len(self.db.execute("SELECT * FROM lists WHERE shortcode=?", \
-                               self.shortcode)):
-            app.reply("The shortcode '%s' is already in use.")
+    def exists(self):
+        cur = self.db.cursor()
+        cur.execute("SELECT * FROM lists WHERE shortcode=?", self.shortcode)
+        if cur.rowcount:
+            return True
+        return False
+
+    def create(self, original_owner):
+        if not self.app.is_valid_shortcode(self.shortcode):
+            self.app.reply("The shortcode you selected is invalid. Please " \
+                           + "choose a number between %d and %d." % \
+                           (self.conf.min_shortcode, self.conf.max_shortcode))
+        elif self.exists():
+            self.app.reply("The shortcode '%s' is already in use." self.shortcode)
         else:
             self.conf.log.info("Creating list %s" % self.shortcode)
-            # TODO: DB
+            cur = self.db.cursor()
+            cur.execute("INSERT INTO lists VALUES (?,?)", self.shortcode, original_owner)
+            self.make_owner(original_owner)
 
     def delete(self, confirmed=False):
         """ If confirmed, remove list from list table, then all associated
         members. Otherwise, save message in confirm_action table.
         """
-        raise NotImplementedError
+        if confirmed:
+            self.conf.log.info("Deleting list %s" % self.shortcode)
+            cur = self.db.cursor()
+            cur.execute("SELECT member FROM membership WHERE list=?", \
+                        self.shortcode)
+            members = cur.fetchall()
+
+            cur.execute("BEGIN TRANSACTION")
+            cur.execute("DELETE FROM lists WHERE shortcode=?", self.shortcode)
+            cur.execute("DELETE FROM membership WHERE list=?", self.shortcode)
+            cur.execute("COMMIT TRANSACTION")
+
+            for m in members:
+                msg = Message(self.conf.app_number, m, None, \
+                              "The list %s has been deleted, and all members" +
+                              " (including you!) have been removed." \
+                              % self.shortcode)
+                self.app.send(msg)
+            self.app.reply("The list %s has been deleted.", self.shortcode)
+        else:
+            self.app.add_pending_action(self.app.msg)
 
     def add_user(self, number):
-        raise NotImplementedError
+        """ Add the specified user to the list """
+        self.conf.log.info("Adding user '%s' to list '%s'" % number, self.shortcode)
+        cur = self.db.cursor()
+        cur.execute("INSERT OR IGNORE INTO membership(list, member) VALUES (?,?)", self.shortcode, number)
+        msg = Message(self.conf.app_number, number, None, "You've been added to the list '%s'." % self.shortcode)
+        self.app.send(msg)
 
     def delete_user(self, number):
         raise NotImplementedError
 
     def make_owner(self, number):
-        raise NotImplementedError
+        raise NotImplementedError 
 
     def unmake_owner(self, number):
         raise NotImplementedError
 
-    def post(self, sender, message):
-        raise NotImplementedError
+    def post(self, message):
+        self.conf.log.info("Posting to list '%s' message: %s" % self.shortcode, message)
+        cur = self.db.cursor()
+        cur.execute("SELECT member FROM membership WHERE list=?", \
+                    self.shortcode)
+        members = cur.fetchall()
+        for m in members:
+            msg = Message(message.sender, m, message.subject, message.body)
+            self.app.send(msg)
+
 
 class Message:
     """ Simple wrapper for a message. Note that ALL fields may be None! """
@@ -81,7 +122,8 @@ class SMSWall:
                 log.debug("Failed to process command: %s" % message)
         elif message.recipient == self.conf.app_number:
             # XXX: should we assert confirmed==False here?
-            self.confirm_action(message.body, message.sender)
+            if "confirm" in message.body:
+                self.confirm_action(message.sender)
         else:
             self.send_to_list(message)
 
@@ -90,21 +132,32 @@ class SMSWall:
         """
         raise NotImplementedError
 
-    def confirm_action(self, actioncode, sender):
-        """ Confirm some previous action. Sensitive actions, like deleting a
+    def confirm_action(self, sender):
+        """ Confirm some pending action. Sensitive actions, like deleting a
         list, may need to be confirmed before they are actually executed. These
         actions are stored in the confirm_action table, which we should
         periodically flush. The stored action is just a command message that
         gets re-submitted to handle_incoming with the 'confirmed' flag set to
-        true.
-
-        Actioncodes are short hashes over the action command, sender, and sent time.
+        true. 
         """
-        raise NotImplementedError
+        # TODO: DB: select confirm_action from pending action for sender
+        confirm_msg = Message(sender, recpipient, None, command)
+        self.handle_incoming(confirm_msg, True)
+
+    def add_pending_action(self, message):
+        """ A user can have up to one pending action. This method generates a
+        confirmation response to the sender. """
+        s = message.sender
+        r = message.recipient
+        command = message.body
+        # TODO: DB: insert sender, recipient, command into pending_actions
+        self.reply("Reply to this message with the word \"confirm\" to " +
+                    "confirm your previous command.")
 
     def send_to_list(self, message):
         """ Send a message to a list. """
-        raise NotImplementedError
+        list_ = message.recipient
+
 
     def clean_confirm_actions(self, age):
         """ Remove all confirm_actions older than age. """
@@ -115,13 +168,17 @@ class SMSWall:
         """
         m = Message(self.conf.app_number, self.msg.sender, None, body)
         self.log.debug("Replying with: %s" % m)
+        # TODO: NOT FINISHED
 
+    def send(self, message):
+        """ Send the specified message. """
+        raise NotImplementedError
 
 class Config:
     def __init__(self, config_dict, logger):
         self.config_dict = config_dict
         self.log = logger
-        self.db = sqlite3.connect(self.db_file)
+        self.db_conn = sqlite3.connect(self.db_file)
         self.log.debug("Connected to DB: %s" % self.db_file)
 
     @property
