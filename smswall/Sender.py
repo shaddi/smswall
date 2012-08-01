239d7a8592514b0701c14984b04578dd44832dce
Shaddi Hasan <shaddih@gmail.com>
2012-08-01 13:26:17 -0700

    Add sender type for testing

--- smswall/Sender.py
+++ smswall/Sender.py
@@ -1,11 +1,25 @@
 import logging
 
 class Sender:
-    def send_sms(sender, receipient, subject, data):
+    def send_sms(self, sender, receipient, subject, data):
         raise NotImplementedError
 
+class TestSender(Sender):
+    """ Saves output to an easily parse-able file format to allow automated
+    verification.
+    """
+
+    def __init__(self):
+        self.msg_count = 0
+        self.logger = logging.getLogger("testsender")
+    def send_sms(self, sender, recipient, subject, data):
+        msg = [ self.msg_count, sender, recipient, subject, data ]
+        self.logger.info("%s" % msg)
+        self.msg_count += 1
+
+
 class LogSender(Sender):
-    def send_sms(sender, recipient, subject, data):
+    def send_sms(self, sender, recipient, subject, data):
         logging.basicConfig(level=logging.DEBUG)
         logging.info("Sent SMS. From: '%s' To: '%s' Subj: '%s' Message: '%s'" \
                      % (sender, recipient, subject, data))
